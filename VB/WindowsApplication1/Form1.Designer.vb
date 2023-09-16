976adbfcc07f95d568287f0d7630487a4c4a8de5
DevExpressExampleBot <fakeEmail>
2023-09-16 13:49:11 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication1/Form1.Designer.vb
+++ VB/WindowsApplication1/Form1.Designer.vb
@@ -1,109 +1,111 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.dxErrorProvider1 = New DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider(Me.components)
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.bindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
-			Me.textEdit1 = New DevExpress.XtraEditors.TextEdit()
-			Me.textEdit2 = New DevExpress.XtraEditors.TextEdit()
-			CType(Me.dxErrorProvider1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.textEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.textEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' dxErrorProvider1
-			' 
-			Me.dxErrorProvider1.ContainerControl = Me
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.Location = New System.Drawing.Point(12, 24)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.Size = New System.Drawing.Size(645, 514)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.UseEmbeddedNavigator = True
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' gridView1
-			' 
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.Name = "gridView1"
-'			Me.gridView1.InvalidRowException += New DevExpress.XtraGrid.Views.Base.InvalidRowExceptionEventHandler(Me.gridView1_InvalidRowException);
-'			Me.gridView1.ValidateRow += New DevExpress.XtraGrid.Views.Base.ValidateRowEventHandler(Me.gridView1_ValidateRow);
-			' 
-			' textEdit1
-			' 
-			Me.textEdit1.Location = New System.Drawing.Point(431, 32)
-			Me.textEdit1.Name = "textEdit1"
-			Me.textEdit1.Size = New System.Drawing.Size(100, 20)
-			Me.textEdit1.TabIndex = 1
-			' 
-			' textEdit2
-			' 
-			Me.textEdit2.Location = New System.Drawing.Point(546, 32)
-			Me.textEdit2.Name = "textEdit2"
-			Me.textEdit2.Size = New System.Drawing.Size(100, 20)
-			Me.textEdit2.TabIndex = 2
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(669, 550)
-			Me.Controls.Add(Me.textEdit2)
-			Me.Controls.Add(Me.textEdit1)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.dxErrorProvider1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.textEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.textEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.dxErrorProvider1 = New DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider(Me.components)
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.bindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+            Me.textEdit1 = New DevExpress.XtraEditors.TextEdit()
+            Me.textEdit2 = New DevExpress.XtraEditors.TextEdit()
+            CType((Me.dxErrorProvider1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.bindingSource1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.textEdit1.Properties), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.textEdit2.Properties), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' dxErrorProvider1
+            ' 
+            Me.dxErrorProvider1.ContainerControl = Me
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.Location = New System.Drawing.Point(12, 24)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.Size = New System.Drawing.Size(645, 514)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.UseEmbeddedNavigator = True
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1})
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            AddHandler Me.gridView1.InvalidRowException, New DevExpress.XtraGrid.Views.Base.InvalidRowExceptionEventHandler(AddressOf Me.gridView1_InvalidRowException)
+            AddHandler Me.gridView1.ValidateRow, New DevExpress.XtraGrid.Views.Base.ValidateRowEventHandler(AddressOf Me.gridView1_ValidateRow)
+            ' 
+            ' textEdit1
+            ' 
+            Me.textEdit1.Location = New System.Drawing.Point(431, 32)
+            Me.textEdit1.Name = "textEdit1"
+            Me.textEdit1.Size = New System.Drawing.Size(100, 20)
+            Me.textEdit1.TabIndex = 1
+            ' 
+            ' textEdit2
+            ' 
+            Me.textEdit2.Location = New System.Drawing.Point(546, 32)
+            Me.textEdit2.Name = "textEdit2"
+            Me.textEdit2.Size = New System.Drawing.Size(100, 20)
+            Me.textEdit2.TabIndex = 2
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(669, 550)
+            Me.Controls.Add(Me.textEdit2)
+            Me.Controls.Add(Me.textEdit1)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.dxErrorProvider1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.bindingSource1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.textEdit1.Properties), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.textEdit2.Properties), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private dxErrorProvider1 As DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private bindingSource1 As System.Windows.Forms.BindingSource
-		Private textEdit2 As DevExpress.XtraEditors.TextEdit
-		Private textEdit1 As DevExpress.XtraEditors.TextEdit
-	End Class
+#End Region
+        Private dxErrorProvider1 As DevExpress.XtraEditors.DXErrorProvider.DXErrorProvider
+
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+
+        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+
+        Private bindingSource1 As System.Windows.Forms.BindingSource
+
+        Private textEdit2 As DevExpress.XtraEditors.TextEdit
+
+        Private textEdit1 As DevExpress.XtraEditors.TextEdit
+    End Class
 End Namespace
-
